12aadb21c38af59228dc5579ee08b4a032b8de54
myb1993 <895133343@qq.com>
2016-03-12 02:41:45 +0800

    Signed-off-by: myb1993 <895133343@qq.com>

---
+++ app/src/main/java/com/example/broadcastbestpractice/BaseActivity.java
@@ -0,0 +1,21 @@
+package com.example.broadcastbestpractice;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2016/3/11.
+ */
+public class BaseActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        ActivityCollector.addActivity(this);
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        ActivityCollector.removeActivity(this);
+    }
+}
